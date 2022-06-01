18f992567edabefdb8e529e012795b1c04cc976d
allthesignals <>
2022-05-31 20:59:58 -0500

    Fix index reset default map position

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,23 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import config from '../config/environment';
+
+const { 'mapbox-gl': mapboxGlConfig } = config;
 
 export default class IndexRoute extends Route {
+  @service
+  mainMap;
+
   model() {
     return this.modelFor('application');
+  }
+
+  afterModel() {
+    this.mainMap.run((map) => {
+      map.flyTo({
+        center: mapboxGlConfig.map.center,
+        zoom: mapboxGlConfig.map.zoom,
+      });
+    });
   }
 }
